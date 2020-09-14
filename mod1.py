d135ec1c631bff43c3eda008f4810bdd9fce164b
Mudathir Lawal <nigercode@gmail.com>
2020-09-14 07:54:29 +0100

    feat: Add  a python test file

---
+++ mod1.py
@@ -0,0 +1,15 @@
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+
+newfilenames = [] 
+
+for i, filename in enumerate(filenames):
+  if filename.endswith('hpp'):  
+    n = len(filename) - 2
+    newfilenames.append(filename[:n])
+    print('{}, {}, {}'.format(i, n, filename))
+  else:
+    newfilenames.append(filename)
+    print('{}, {}, {}'.format(i, '-', filename))
+
+print('{} {}'.format('\n', filenames))    
+print('{} {}'.format('\n', newfilenames))  
